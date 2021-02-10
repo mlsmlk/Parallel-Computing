f82adb4b0c7eabb1300a58e91424ba14bc9f41be
Haluk <46542257+halukcalin@users.noreply.github.com>
2021-02-10 17:10:55 -0500

    Dining Philosophers can keep thinking... In progress

--- Assignment1/src/ca/mcgill/ecse420/a1/DiningPhilosophers.java
+++ Assignment1/src/ca/mcgill/ecse420/a1/DiningPhilosophers.java
@@ -8,17 +8,49 @@ public class DiningPhilosophers {
 	
 	public static void main(String[] args) {
 
-		int numberOfPhilosophers = 5;
+
+			int numberOfPhilosophers = 5;
+
                 Philosopher[] philosophers = new Philosopher[numberOfPhilosophers];
                 Object[] chopsticks = new Object[numberOfPhilosophers];
 	}
 
+	void put_fork(int num) {
+
+	}
+
+	void take_fork(int num) {
+
+	}
+
+	void thinking(String action) throws InterruptedException {
+		System.out.println(Thread.currentThread().getName() + " " + action);
+		Thread.sleep(((int) (Math.random() * 100)));
+	}
+
 	public static class Philosopher implements Runnable {
+
+		private Object leftChopstick;
+		private Object rightChopstick;
+
+		public Philosopher(Object leftChopstick, Object rightChopstick ) {
+			this.leftChopstick = leftChopstick;
+			this.rightChopstick = rightChopstick;
+		}
 
 		
 
 		@Override
 		public void run() {
+
+			while(true) {   //while running
+				//do stuff, thinking()?
+				//take_fork(i)->left fork
+				//take_fork((i+1)%N)-> right fork. n is number of forks
+				//EAT()
+				//put fork(i)
+				//put fork((i+1)%N)--> mod n
+			}
 			
 		}
 
@@ -26,3 +58,12 @@ public class DiningPhilosophers {
 	}
 
 }
+
+// 2 states-> thinking and eating
+
+
+
+//add some priority, have a queue
+//locking needs to be atomic
+//when one program is asking, the other cannot gain it
+//
