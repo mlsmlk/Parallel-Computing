cbc391bb05fb5b0f52dfe7f93b6eda76a0fd9420
Haluk <46542257+halukcalin@users.noreply.github.com>
2021-04-18 19:13:45 -0400

    Add files via upload

--- Assignment3/src/ca/mcgill/ecse420/a3/FineGrainedAlgorithmTest.java
+++ Assignment3/src/ca/mcgill/ecse420/a3/FineGrainedAlgorithmTest.java
@@ -4,16 +4,16 @@ public class FineGrainedAlgorithmTest {
 
     public static void main(String[] args){
         FineGrainedAlgorithm<Integer> newList = new FineGrainedAlgorithm<Integer>();
+        newList.add(8);
+        newList.add(7);
         newList.add(3);
-        newList.add(6);
-        newList.add(9);
-        newList.add(4);
-        newList.add(43);
-        newList.add(57);
-        System.out.println("\nCheck contains() for 3:\nExpected: true\nObtained: " + newList.contains(3));
-        System.out.println("\nCheck contains() for 41:\nExpected: false\nObtained: "+ newList.contains(41));
-        System.out.println("\nCheck contains() for 57:\nExpected: true\nObtained: " + newList.contains(57));
-        System.out.println("\nCheck contains() for 513:\nExpected: false\nObtained: "+ newList.contains(513));
+        newList.add(15);
+        newList.add(34);
+        newList.add(105);
+        System.out.println("\nCheck contains() for 8:\nExpected: true\nObtained: " + newList.contains(8));
+        System.out.println("\nCheck contains() for 14:\nExpected: false\nObtained: "+ newList.contains(14));
+        System.out.println("\nCheck contains() for 15:\nExpected: true\nObtained: " + newList.contains(15));
+        System.out.println("\nCheck contains() for 205:\nExpected: false\nObtained: "+ newList.contains(205));
     }
 
 }
